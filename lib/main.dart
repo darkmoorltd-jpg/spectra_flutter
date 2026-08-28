89de122cf0facf63baa3d5ed013a1ea7b63c215a
darkmoorltd-jpg <darkmoorltd@gmail.com>
2026-08-28 14:18:01 +0100

    Add all routes and Supabase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/login_screen.dart';
 import 'screens/dashboard_screen.dart';
 import 'screens/scan_screen.dart';
+import 'screens/market_screen.dart';
+import 'screens/vault_screen.dart';
+import 'screens/result_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://mzxbndfmeuewmbhwiotc.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16eGJuZGZtZXVld21iaHdpb3RjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NjA2MzEsImV4cCI6MjEwMzMzNjYzMX0.QRpTunoZ2bPGYSu5qwWuq8g6G1KjxtoiQ3vTZLWcUvk',
+  );
   runApp(const SpectraApp());
 }
 
@@ -36,6 +45,8 @@ class SpectraApp extends StatelessWidget {
         '/login': (context) => LoginScreen(),
         '/dashboard': (context) => DashboardScreen(),
         '/scan': (context) => ScanScreen(),
+        '/market': (context) => MarketScreen(),
+        '/vault': (context) => VaultScreen(),
       },
     );
   }
